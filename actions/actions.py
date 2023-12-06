6df5964a5cddf101baf3537fe11f66371a8a8f66
dmKonstant <dimitris.konstantakopoulos@lastmily.com>
2023-12-06 21:00:32 +0200

    comitin changes sub rasa

--- actions/actions.py
+++ actions/actions.py
@@ -10,8 +10,9 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import requests
-
-
+from rasa_sdk.events import ActionExecuted
+import os
+from rasa_sdk.events import SlotSet
 class ActionHaystack(Action):
 
     def name(self) -> Text:
@@ -36,3 +37,45 @@ class ActionHaystack(Action):
         dispatcher.utter_message(text=answer)
 
         return []
+
+
+class ActionRequestHuman(Action):
+    def name(self):
+        return 'action_request_human'
+
+    def run(self, dispatcher, tracker, domain):
+        print("I REQUEST HUMAN AND I SEND TO CHANNEL TO CONNECT TO SOMEONE ELSE")
+        return []
+
+
+class ActionSessionStart(Action):
+    def name(self):
+        return "action_session_start"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Add your code or actions that need to be performed at the start of the conversation here
+        custom_data = tracker.get_slot("language2")
+        print(custom_data)
+        # print("I SET LANGUAGE111111111111 TO : " + custom_data)
+        language_value = custom_data
+        # language_value = tracker.get_slot("language2") # Replace with your environment variable name # Replace with your environment variable name
+        print("I SET LANGUAGE TO : "+language_value)
+        # language_value = 'Greek'
+        if language_value in ["English", "Greek"]:
+            return [SlotSet("language", language_value)]
+        return [ActionExecuted("action_session_start")]
+
+class SetLanguageSlotAction(Action):
+    def name(self) -> Text:
+        return "action_set_language_slot"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Fetch the value of the environment variable
+        language_value = tracker.get_slot("language") # Replace with your environment variable name
+        # language_value = 'Greek'
+        if language_value in ["English", "Greek"]:
+            return [SlotSet("language", language_value)]
+
+
---
+++ dockerfile
@@ -0,0 +1,23 @@
+FROM rasa/rasa:latest
+WORKDIR '/app'
+COPY . /app
+USER root
+# WORKDIR /app
+# COPY . /app
+COPY ./data /app/data
+
+COPY requirements.txt .
+VOLUME /app
+VOLUME /app/data
+VOLUME /app/models
+RUN apt-get update && \
+    apt-get install -y build-essential
+
+
+
+RUN pip install --upgrade pip setuptools
+RUN pip install --verbose --no-cache-dir -r requirements.txt
+RUN python -m spacy download en_core_web_md
+RUN rasa train
+
+CMD ["run","-m","/app/models","--enable-api","--cors","*","--debug" ,"--endpoints", "endpoints.yml", "--log-file", "out.log", "--debug"]
